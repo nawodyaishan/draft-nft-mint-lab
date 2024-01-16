c38c18603820f182be95aadb3e10cd652721cadb
Nawodya Ishan <nawodyain@gmail.com>
2024-01-16 16:25:37 +0530

    feat: add DraftMultiMinter contract

--- contracts/DraftMinter.sol
+++ contracts/DraftMinter.sol
@@ -4,18 +4,33 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DraftMinter is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract DraftMinter is
+ERC721,
+ERC721Enumerable,
+ERC721URIStorage,
+ERC721Pausable,
+Ownable
+{
     uint256 private _nextTokenId;
 
-    constructor()
-    ERC721("DraftMinter", "DRFT")
-    Ownable(msg.sender)
-    {}
+    constructor(
+        address initialOwner
+    ) ERC721("DraftMinter", "DRFT") Ownable(msg.sender) {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://turquoise-rear-loon-357.mypinata.cloud/ipfs/QmU9pYR9CxUN1DaHENo4iVcoGJJ13ZGCxQ5mCAqdtC7CdU/";
+        return
+            "https://turquoise-rear-loon-357.mypinata.cloud/ipfs/QmU9pYR9CxUN1DaHENo4iVcoGJJ13ZGCxQ5mCAqdtC7CdU/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
     }
 
     function safeMint(address to, string memory uri) public onlyOwner {
@@ -24,33 +39,34 @@ contract DraftMinter is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         _setTokenURI(tokenId, uri);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(address to, uint256 tokenId, address auth)
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    )
     internal
-    override(ERC721, ERC721Enumerable)
+    override(ERC721, ERC721Enumerable, ERC721Pausable)
     returns (address)
     {
         return super._update(to, tokenId, auth);
     }
 
-    function _increaseBalance(address account, uint128 value)
-    internal
-    override(ERC721, ERC721Enumerable)
-    {
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
         super._increaseBalance(account, value);
     }
 
-    function tokenURI(uint256 tokenId)
-    public
-    view
-    override(ERC721, ERC721URIStorage)
-    returns (string memory)
-    {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
+    function supportsInterface(
+        bytes4 interfaceId
+    )
     public
     view
     override(ERC721, ERC721Enumerable, ERC721URIStorage)
